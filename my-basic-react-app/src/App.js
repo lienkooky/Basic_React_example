08933a9898d9e7dd51f05c1aa941d67b866ed055
GEONYEONG SON <thsrjs5@gmail.com>
2021-08-13 22:31:24 +0900

    create state

--- my-basic-react-app/src/App.js
+++ my-basic-react-app/src/App.js
@@ -4,16 +4,35 @@ import Content from './components/Content';
 import Subject from './components/Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      welcome: { title: 'welcome', sub: 'hello react!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
+      ],
+    };
+  }
   render() {
+    const { subject, contents, mode, welcome } = this.state;
+    let _title,
+      _desc = null;
+    if (mode === 'welcome') {
+      _title = mode.title;
+      _desc = welcome.sub;
+    } else if (mode === 'read') {
+      _title = contents[0].title;
+      _desc = contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="REACT" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          desc=" HTML is HyperText Markup Language."
-        ></Content>
+        <Subject title={subject.title} sub={subject.sub}></Subject>
+        <TOC data={contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
